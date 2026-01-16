d164ac1fbd6d7d412991f8b935e4513890b4010c
Jochen Klar <mail@jochenklar.de>
2026-01-16 14:41:44 +0100

    Use ROR API v2

--- rdmo_ror/providers.py
+++ rdmo_ror/providers.py
@@ -2,6 +2,7 @@ import re
 
 from django.conf import settings
 from django.templatetags.static import static
+from django.utils.translation import get_language
 
 import requests
 
@@ -15,7 +16,7 @@ class RorProvider(Provider):
 
     def get_options(self, project, search=None, user=None, site=None):
         if search:
-            url = getattr(settings, 'ROR_PROVIDER_URL', 'https://api.ror.org/v1/').rstrip('/')
+            url = getattr(settings, 'ROR_PROVIDER_URL', 'https://api.ror.org/v2/').rstrip('/')
             headers = getattr(settings, 'ROR_PROVIDER_HEADERS', {})
 
             response = requests.get(url + '/organizations', params={
@@ -42,11 +43,28 @@ class RorProvider(Provider):
         return item.get('id', '').replace('https://ror.org/', '')
 
     def get_text(self, item):
-        ror_name = item.get('name', '')
         ror_id = item['id']
+        ror_name = self.get_name(item)
         ror_img = static('ror/img/ROR.png')
         ror_link = f'<a href="{ror_id}"><img height="16" src="{ror_img}" alt="ROR logo" /> {ror_id}</a>'
-        return f'{ror_name} {ror_link}'
+        return f'{ror_name} {ror_link}' if ror_name else ror_link
+
+    def get_name(self, item):
+        current_language = get_language()
+        labels = [
+            name
+            for name in item.get('names', [])
+            if 'label' in name.get('types', [])
+        ]
+
+        if not labels:
+            return None
+
+        for name in labels:
+            if name.get('lang') == current_language:
+                return name.get('value')
+
+        return labels[0].get('value')
 
     def get_search(self, search):
         # reverse get_text to perform the search, remove everything after [
